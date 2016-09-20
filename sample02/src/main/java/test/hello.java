802d51d52e452bf350c1380c0e166635d3de41a5
BitCamp <BitCamp@BitCamp-PC>
2016-09-20 10:44:22 +0900

    1

--- sample02/src/main/java/test/hello.java
+++ sample02/src/main/java/test/hello.java
@@ -3,7 +3,10 @@ package test;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 9; i++) {
+			System.out.printf("%d 뀨?",i);
+		}
 
 	}
 
