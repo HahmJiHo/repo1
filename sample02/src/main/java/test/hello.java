7c484a061f52ba91b5fbf30989c7d6481b2545a7
BitCamp <BitCamp@BitCamp-PC>
2016-09-20 11:20:08 +0900

    .

--- sample02/src/main/java/test/hello.java
+++ sample02/src/main/java/test/hello.java
@@ -10,9 +10,7 @@ public class hello {
 		if ( age < 20) {
 		  System.out.println("함지호바보.");
 		}
-		int c;
 		String abc;
-	  int age = 30;
 	  if (age < 20) {
 	    System.out.println("성년입니다.");
 	  } else {
