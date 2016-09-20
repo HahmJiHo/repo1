cc57928d2f054d3c202eddf98c32294297e86db7
mun sy <mun sy@munsy-PC>
2016-09-20 10:47:28 +0900

    .

--- sample02/src/main/java/test/hello.java
+++ sample02/src/main/java/test/hello.java
@@ -3,7 +3,11 @@ package test;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int age = 20;
+		if ( age < 20) {
+		  System.out.println("성년");
+		}
+		
 
 	}
 
