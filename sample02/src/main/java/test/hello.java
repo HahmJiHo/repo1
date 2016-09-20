0a13600cd602aaf5f134817e7a53fb80525be3f2
CheolHan <joker10421@gmail.com>
2016-09-20 10:45:47 +0900

    .

--- sample02/src/main/java/test/hello.java
+++ sample02/src/main/java/test/hello.java
@@ -3,8 +3,12 @@ package test;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	  int age = 30;
+	  if (age < 20) {
+	    System.out.println("성년입니다.");
+	  } else {
+	    System.out.println("???");
+	  }
 	}
 
 }
