79f4ed44aedcd426a2504943b3dddaf707d5feef
mun sy <mun sy@munsy-PC>
2016-09-20 11:14:34 +0900

    .

--- sample02/src/main/java/test/hello.java
+++ sample02/src/main/java/test/hello.java
@@ -3,29 +3,21 @@ package test;
 public class hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 		int age = 20;
 		if ( age < 20) {
 		  System.out.println("함지호바보.");
 		}
 		
-=======
 	  int age = 30;
 	  if (age < 20) {
 	    System.out.println("성년입니다.");
 	  } else {
 	    System.out.println("???");
 	  }
->>>>>>> branch 'master' of https://github.com/HahmJiHo/repo1.git
-=======
 		
 		for (int i = 0; i < 9; i++) {
 			System.out.printf("%d 뀨?",i);
 		}
->>>>>>> branch 'master' of https://github.com/HahmJiHo/repo1.git
-
->>>>>>> branch 'master' of https://github.com/HahmJiHo/repo1.git
 	}
 
 }
